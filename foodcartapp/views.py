3b46fe5ea349532ebe3964b3a99cf216f3b68e3d
pzhuchkov <pzhuchkov@gmail.com>
2021-02-19 00:19:33 +0300

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -9,6 +9,7 @@ from .models import Product
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 def banners_list_api(request):
@@ -65,14 +66,22 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-    order_request = request.data
-    order = Order.objects.create(firstname=order_request['firstname'],
-                                 lastname=order_request['lastname'],
-                                 phonenumber=order_request['phonenumber'],
-                                 address=order_request['address'])
-    for order_item in order_request['products']:
-        product = Product.objects.get(id=order_item['product'])
-        OrderItem.objects.create(order=order,
-                                 product=product,
-                                 quantity=order_item['quantity'])
-    return Response({'ok': True})
+    received_order = request.data
+    if 'products' not in received_order:
+        return Response({'error': 'products key is not presented or not list'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    products = received_order['products']
+    if isinstance(products, list) and products:
+        order = Order.objects.create(firstname=received_order['firstname'],
+                                     lastname=received_order['lastname'],
+                                     phonenumber=received_order['phonenumber'],
+                                     address=received_order['address'])
+        for order_item in products:
+            product = Product.objects.get(id=order_item['product'])
+            OrderItem.objects.create(order=order,
+                                     product=product,
+                                     quantity=order_item['quantity'])
+        return Response({'ok': True})
+    else:
+        return Response({'error': 'products key is not presented or not list'},
+                        status=status.HTTP_400_BAD_REQUEST)
