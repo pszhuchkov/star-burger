0112129778edf6ad192c4c1e915ddbf71c9ee3bd
pzhuchkov <pzhuchkov@gmail.com>
2021-02-20 11:18:02 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http import JsonResponse
 from django.templatetags.static import static
 
@@ -67,21 +65,39 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     received_order = request.data
-    if 'products' not in received_order:
+
+    if 'products' not in received_order or not received_order['products'] or not isinstance(received_order['products'], list):
         return Response({'error': 'products key is not presented or not list'},
                         status=status.HTTP_400_BAD_REQUEST)
-    products = received_order['products']
-    if isinstance(products, list) and products:
-        order = Order.objects.create(firstname=received_order['firstname'],
-                                     lastname=received_order['lastname'],
-                                     phonenumber=received_order['phonenumber'],
-                                     address=received_order['address'])
-        for order_item in products:
-            product = Product.objects.get(id=order_item['product'])
-            OrderItem.objects.create(order=order,
-                                     product=product,
-                                     quantity=order_item['quantity'])
-        return Response({'ok': True})
-    else:
-        return Response({'error': 'products key is not presented or not list'},
+    if 'firstname' not in received_order or not received_order['firstname'] or not isinstance(received_order['firstname'], str):
+        return Response({'error': 'firstname key is not presented or not str'},
                         status=status.HTTP_400_BAD_REQUEST)
+    if 'lastname' not in received_order or not received_order['lastname'] or not isinstance(received_order['lastname'], str):
+        return Response({'error': 'lastname key is not presented or not str'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    if 'phonenumber' not in received_order or not received_order['phonenumber'] or not isinstance(received_order['phonenumber'], str):
+        return Response({'error': 'phonenumber key is not presented or not str'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    if 'address' not in received_order or not received_order['address'] or not isinstance(received_order['address'], str):
+        return Response({'error': 'address key is not presented or not str'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    order = Order.objects.create(firstname=received_order['firstname'],
+                                 lastname=received_order['lastname'],
+                                 phonenumber=received_order['phonenumber'],
+                                 address=received_order['address'])
+
+    order_products = received_order['products']
+
+    for order_product in order_products:
+        if 'product' not in order_product or not isinstance(order_product['product'], int):
+            return Response({'error': 'product is not presented or is not int'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        product = Product.objects.get(id=order_product['product'])
+        if not product:
+            return Response({'error': 'product doesnt exist'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        OrderItem.objects.create(order=order,
+                                 product=product,
+                                 quantity=order_product['quantity'])
+    return Response({'ok': True})
