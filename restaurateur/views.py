9d8e3c63943728da77071f396ea907a637479446
pzhuchkov <pzhuchkov@gmail.com>
2021-02-25 13:13:30 +0300

    Rename order_items to order

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -97,6 +97,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    orders = Order.objects.fetch_with_order_price()
     return render(request, template_name='order_items.html', context={
-        'order_items': Order.objects.fetch_with_order_price()
+        'orders': orders
     })
