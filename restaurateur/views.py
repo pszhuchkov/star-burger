d4c2a8e9237935fa20ebeb0160dea9c09bca9738
pzhuchkov <pzhuchkov@gmail.com>
2021-02-26 18:04:11 +0300

    Add function to fetch coordinates

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -125,6 +125,17 @@ def get_product_ids(order):
     return product_ids
 
 
+def fetch_coordinates(apikey, place):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    params = {"geocode": place, "apikey": apikey, "format": "json"}
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
 
